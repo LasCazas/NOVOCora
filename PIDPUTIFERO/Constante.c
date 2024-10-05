d0853f024b54d0c0958c174b4c25d009da52655b
LasCazas <132779159+LasCazas@users.noreply.github.com>
2024-10-05 12:17:51 -0300

    Update Constante.c

--- PIDPUTIFERO/Constante.c
+++ PIDPUTIFERO/Constante.c
@@ -7,8 +7,6 @@
 
 #define Delta 20
 #define DifTras 18
-const int Branco = 1;
-const int Preto = 0;
 //////////////////////////////////////// BOTOES ////////////////////////////////////////
 #define BotCalibra A3
 #define BotStart A0
@@ -17,20 +15,21 @@ const int Preto = 0;
 #define QTSensores 11
 #define MUX_SIG A2
 const int MUX_S[4] = {2, 3, 4, 5}; // Pinos do mux {A,B,C,D}
-#define BRANCO 0
-#define PRETO 1
+#define BRANCO 1
+#define PRETO 0
 #define NumLeituras 5
 //////////////////////////////////////// MOTOR ////////////////////////////////////////
 #define ENA 6 // Esquerda
-#define IN1 8 // Esquerda
-#define IN2 7 // Esquerda
-#define IN3 10 // Direita
-#define IN4 11 // Direita
+#define IN1 7 // Esquerda
+#define IN2 8 // Esquerda
+#define IN3 11 // Direita
+#define IN4 10 // Direita
 #define ENB 12 // Direita
+#define STBY 9
 
-#define PWME 80
-#define PWMD 80
-#define MAXR 80
+#define PWME 130
+#define PWMD 130
+#define MAXR 130
 //////////////////////////////////////// AUXLIARES ////////////////////////////////////////
 #define Parar 0
 #define Frente 1
