27bc540b09f5a97f4cba40861e9517d0caaf9ba2
LasCazas <132779159+LasCazas@users.noreply.github.com>
2024-10-06 21:37:40 -0300

    Update Constante.c

--- LeituraQuadrados/Constante.c
+++ LeituraQuadrados/Constante.c
@@ -7,39 +7,32 @@
 
 #define Delta 20
 #define DifTras 18
-const int Branco = 1;
-const int Preto = 0;
 //////////////////////////////////////// BOTOES ////////////////////////////////////////
-#define BotCalibra 1
-#define BotStart 1
+#define BotCalibra A3
+#define BotStart A0
+#define BUZZ A1
 //////////////////////////////////////// SENSOR ////////////////////////////////////////
 #define QTSensores 11
-#define MUX_SIG 1
-const int MUX_S[4] = {1, 1, 1, 1}; // Inicializa o array com valores
-#define BRANCO 0
-#define PRETO 1
-#define NumLeituras 5 // Número de leituras para cálculo da média
-//////////////////////////////////////// Square ////////////////////////////////////////
-#define BUFFER_SIZE 5
-
+#define MUX_SIG A2
+const int MUX_S[4] = {5, 4, 3, 2}; // Pinos do mux {A,B,C,D}
+#define BRANCO 1
+#define PRETO 0
+#define NumLeituras 5
 //////////////////////////////////////// MOTOR ////////////////////////////////////////
-#define ENA 3 // Esquerda
-#define IN1 5 // Esquerda
-#define IN2 4 // Esquerda
-#define IN3 7 // Direita
-#define IN4 6 // Direita
-#define ENB 9 // Direita
+#define ENA 6 // Esquerda
+#define IN1 7 // Esquerda
+#define IN2 8 // Esquerda
+#define IN3 11 // Direita
+#define IN4 12 // Direita
+#define ENB 10 // Direita
+#define STBY 9
 
 #define PWME 80
 #define PWMD 80
-#define MAXR 80
-
+#define MAXR 60
+//////////////////////////////////////// Square ////////////////////////////////////////
+#define BUFFER_SIZE 5
 #define re 1
 #define PraFrente 0
-//////////////////////////////////////// AUXLIARES ////////////////////////////////////////
-#define Parar 0
-#define Frente 1
-#define Direita 2
-#define Esquerda 3
 
 #endif // CONSTANTES_H
